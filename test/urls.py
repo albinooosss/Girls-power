22f1c4cf9c8f5ce6e73ef4e3640882698a6c0e65
Adelina <adelinasafarova84@gmail.com>
2024-07-02 20:18:59 +0300

    сделала формы создания и редоктирования теста

--- test/urls.py
+++ test/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', views.test_list, name='test_list'),
     path('test/create_test/', views.create_test, name='create_test'),
     path('test/passing_the_test/<int:pk>/', views.passing_the_test, name='passing_the_test'),
+    path('test/<int:pk>/edit_test/', views.edit_test, name='edit_test'),
 
 ]
--- test/views.py
+++ test/views.py
@@ -8,6 +8,7 @@ def test_list(request):
     tests = Test
     return render(request, 'test/test_list.html', {"tests": tests})
 
+
 def create_test(request):
     if request.method == "POST":
         form = TestForm(request.POST)
@@ -20,6 +21,21 @@ def create_test(request):
         form = TestForm()
     return render(request, 'test/create_test.html', {'form': form})
 
+
 def passing_the_test(request, pk):
     test = get_object_or_404(Test, pk=pk)
     return render(request, 'test/passing_the_test.html', {'test': test})
+
+
+def edit_test(request, pk):
+    test = get_object_or_404(Test, pk=pk)
+    if request.method == "POST":
+        form = TestForm(request.POST, instance=test)
+        if form.is_valid():
+            test = form.save(commit=False)
+            test.author = request.user
+            test.save()
+            return redirect('post_detail', pk=test.pk)
+    else:
+        form = TestForm(instance=test)
+    return render(request, 'test/edit_test.html', {'form': form})
