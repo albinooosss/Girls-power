7453b5e35bcc8fb84d5d6d55f837cf65f813b0c2
Adelina <adelinasafarova84@gmail.com>
2024-07-02 14:53:32 +0300

    added some views

--- test/urls.py
+++ test/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.test_list, name='test_list'),
-    path('test/new/', views.test_new, name='test_new'),
+    path('test/new/', views.create_test(), name='test_new'),
+    path('test/<int:pk>/', views.passing_the_test(), name='test_detail'),
+
 ]
--- test/views.py
+++ test/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Test
 from .forms import TestForm
 
@@ -7,8 +7,12 @@ def test_list(request):
     tests = Test
     return render(request, 'test/test_list.html', {"tests": tests})
 
-def test_new(request):
+def create_test(request):
     form = TestForm()
-    return render(request, 'test/test_edit.html', {'form': form})
+    return render(request, 'test/create_test.html', {'form': form})
+
+def passing_the_test(request, pk):
+    test = get_object_or_404(Test, pk=pk)
+    return render(request, 'test/passing_the_test.html', {'test': test})
 
 
